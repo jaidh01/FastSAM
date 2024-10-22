205f3f452e314a8f4f48c3a498700bdcaa075b4d
Jai Dhingra <117927011+jaidh01@users.noreply.github.com>
2024-10-22 18:22:26 +0530

    Update prompt.py

--- fastsam/prompt.py
+++ fastsam/prompt.py
@@ -444,7 +444,7 @@ class FastSAMPrompt:
         format_results = self._format_results(self.results[0], 0)
         cropped_boxes, cropped_images, not_crop, filter_id, annotations = self._crop_image(format_results)
         clip_model, preprocess = clip.load('ViT-B/32', device=self.device)
-        scores = self.retrieve(clip_model, preprocess, cropped_boxes, text, device=device)
+        scores = self.retrieve(clip_model, preprocess, cropped_boxes, text, device=self.device)
         max_idx = scores.argsort()
         max_idx = max_idx[-1]
         max_idx += sum(np.array(filter_id) <= int(max_idx))
